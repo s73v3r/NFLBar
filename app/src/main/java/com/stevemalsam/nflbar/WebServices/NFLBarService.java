cd74166caffa43e7a77ed9f9be22c9b953f8b6f3
Steve Malsam <stevemalsam@stevemalsam.com>
2014-07-09 19:10:32 -0700

    Added Retrofit; set up webservice

---
+++ app/src/main/java/com/stevemalsam/nflbar/WebServices/NFLBarService.java
@@ -0,0 +1,17 @@
+package com.stevemalsam.nflbar.WebServices;
+
+import com.stevemalsam.nflbar.Models.Venue;
+
+import java.util.List;
+
+import retrofit.http.GET;
+
+/**
+ * Created by stevo on 7/9/14.
+ */
+public interface NFLBarService {
+    public static final String URL = "https://s3.amazonaws.com";
+
+    @GET("/jon-hancock-phunware/nflapi-static.json")
+    List<Venue> listVenues();
+}
